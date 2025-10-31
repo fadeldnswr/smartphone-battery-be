0ca351d9eedbe69b8c3a4f2f823444774796d27d
fadeldnswr <fadel.achmad@ui.ac.id>
2025-10-31 08:50:00 +0700

    Revised supabase controller function

--- src/api/controller/db_controller.py
+++ src/api/controller/db_controller.py
@@ -6,7 +6,6 @@ import os
 import sys
 
 from supabase import create_client, Client
-from src.logging.logging import logging
 from src.exception.exception import CustomException
 from dotenv import load_dotenv
 load_dotenv()
@@ -24,50 +23,5 @@ def create_supabase_connection() -> Client:
       os.getenv("SUPABASE_API_KEY")
     )
     return supabase
-  except Exception as e:
-    raise CustomException(e, sys)
-
-# Define function to fetch data from database
-def get_data_from_db(table:str, column_name:str):
-  '''
-  Function to fetch data from Supabase database\n
-  returns: 
-  - List of records from the database
-  '''
-  try:
-    # Initialize supabase connection
-    logging.info("Fetching data from database...")
-    supabase = create_supabase_connection()
-    
-    # Fetch latest data from supabase table
-    response = supabase.table(table) \
-    .select(column_name).order("created_at", desc=True) \
-    .limit(1).execute()
-    
-    # Return fetched data
-    data = response.data
-    logging.info("Data fetched successfully.")
-    return data
-  except Exception as e:
-    raise CustomException(e, sys)
-
-# Define function to insert data into database
-def insert_data_to_db(data, table_name):
-  '''
-  Function to insert data into Supabase database\n
-  parameters:
-  - data (list of dicts): Data to be inserted
-  \nreturns: 
-  - Response from the insert operation
-  '''
-  try:
-    # Initialize supabase connection
-    logging.info("Inserting data into database...")
-    supabase = create_supabase_connection()
-    
-    # Insert data into supabase table
-    response = supabase.table(table_name).insert(data).execute()
-    logging.info("Data inserted successfully.")
-    return response
   except Exception as e:
     raise CustomException(e, sys)
