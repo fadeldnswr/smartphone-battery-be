e98aa6aa3d57591b8f507c239d62a975b942fda3
fadeldnswr <fadel.achmad@ui.ac.id>
2025-11-01 19:57:25 +0700

    Create query params for table name and device id

--- src/api/routes/data_visualization.py
+++ src/api/routes/data_visualization.py
@@ -9,7 +9,7 @@ from fastapi import APIRouter, HTTPException, Query
 from typing import List, Dict, Any
 
 from src.logging.logging import logging
-from src.api.controller.db_controller import get_data_from_db
+from src.api.controller.db_controller import create_supabase_connection 
 
 # Define router instance
 router = APIRouter()
@@ -17,16 +17,28 @@ router = APIRouter()
 # Define data retrieval route
 @router.get("/metrics", status_code=200)
 async def visualize_data_from_smartphone(
-  table_name:str = Query(..., description="Fetch latest record for specific table_name")
+  table_name:str = Query(..., description="Fetch latest record for specific table_name"),
+  device_id:str = Query(..., description="Device ID to filter data")
   ) -> Dict[List, Any]:
   try:
     logging.info("Get data from database..")
-    raw_data = get_data_from_db(table=table_name)
+    supabase = create_supabase_connection()
+    
+    # Fetch last raw data from specified table
+    logging.info(f"Fetching raw data from table: {table_name}...")
+    raw_data_response = (
+      supabase.table(table_name=table_name)
+      .select("*")
+      .eq("device_id", device_id)
+      .order("created_at", desc=True)
+      .limit(1)
+      .execute()
+    )
     
     logging.info("Data retrieved successfully.")
     return {
       "message": "Data has been retrieved successfully",
-      "data": raw_data
+      "data": raw_data_response.data
     }
   except Exception as e:
     raise HTTPException(status_code=500, detail=str(e))
